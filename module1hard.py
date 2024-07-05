c7043f09a6de5f80f84dfba5fe04fea2030619a6
Dokka Ilaev <105655422+Tommyast@users.noreply.github.com>
2024-07-05 17:31:56 +0300

    Add files via upload
    Fixed version of my hw

--- module1hard.py
+++ module1hard.py
@@ -7,21 +7,8 @@ grades_calc = [sum(grades[0]) / len(grades[0]),
                sum(grades[3]) / len(grades[3]),
                sum(grades[4]) / len(grades[4])]
 
-students_average_grades = {'Johnny': grades_calc[0],
-                           'Bilbo': grades_calc[1],
-                           'Steve': grades_calc[2],
-                           'Kendrick': grades_calc[3],
-                           'Aaron': grades_calc[4]}
+students_average_grades = {student: grades_calc[i] for i, student in enumerate(sorted(students))}
 
 print(students_average_grades)
 
-# Первый способ решения. Изменил в последствии на текущий.
 
-# johnny = sum(grades[0])/len(grades[0])
-# bilbo = sum(grades[1])/len(grades[1])
-# steve = sum(grades[2])/len(grades[2])
-# kendrick = sum(grades[3])/len(grades[3])
-# aaron = sum(grades[4])/len(grades[4])
-#
-# a = {'Johnny:' :johnny,'Bilbo:' : bilbo, 'Steve:' : steve, 'Kendrick:' : kendrick, 'Aaron:' : aaron}
-# print(a)
