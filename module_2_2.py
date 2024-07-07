5d1dc836de5cf43511416abb70b065035c54e2a3
Dokka Ilaev <105655422+Tommyast@users.noreply.github.com>
2024-07-07 23:53:53 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,22 @@
+first = 981
+second = 631
+third = 112
+
+if first == second and second == third:
+    print("3")
+elif first == second or second == third or first == third:
+    print("2")
+else:
+    print("0")
+
+
+first = 333
+second = 612
+third = 333
+
+if first == second and second == third:
+    print("3")
+elif first == second or second == third or first == third:
+    print("2")
+else:
+    print("0")
