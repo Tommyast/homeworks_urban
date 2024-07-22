0b50d3f2e15f65c2657386d3fb4c532b44fa7732
Dokka Ilaev <105655422+Tommyast@users.noreply.github.com>
2024-07-22 10:16:48 +0300

    Update module_3_2.py
    исправленная версия

--- module_3_2.py
+++ module_3_2.py
@@ -1,10 +1,16 @@
 def send_email(message, recipient, *, sender="university.help@gmail.com"):
     if "@" not in sender and ".com" not in sender and ".ru" not in sender and ".net" not in sender:
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+        return
+    if "@" not in recipient and (".com" not in recipient or ".ru" not in recipient or ".net" not in recipient):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+        return
     if sender == recipient:
         print("Нельзя отправить письмо самому себе!")
+        return
     if sender == "university.help@gmail.com":
         print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+        return
     else:
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
 
@@ -13,3 +19,4 @@ send_email('Это сообщение для проверки связи', 'vasy
 send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
 send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
 send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
